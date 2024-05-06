689326b40b8badbadb4f81c418dbc5fe4e3d99c4
PopCat <mtvister155@gmail.com>
2024-05-06 22:41:37 +0500

    userDashboardHeader has been updated

--- Law-simulator/src/components/UserDashboard.vue
+++ Law-simulator/src/components/UserDashboard.vue
@@ -1,26 +1,31 @@
 <script>
 import { mapGetters } from 'vuex';
+import DashboardHeader from './subcomponents/DashboardHeader.vue';
 
     export default{
+        components:{DashboardHeader},
         computed: mapGetters(['getUserName','getUserSurname','getUserEmail','getUserPassword']),
-
-        data(){
-            return{
-
-            }
-        }
 }
 </script>
 
 <template>
-<p>ВЫ ПРОШЛИ АВТОРИЗАЦИЮ</p>
-<p>Ваше имя {{ this.$store.getters.getUserName }}</p>
-<p>Ваша Фамилия {{ this.$store.getters.getUserSurname }}</p>
-<p>Ваша почта {{ this.$store.getters.getUserEmail }}</p>
-<p>Ваш пароль {{ this.$store.getters.getUserPassword }}</p>
+    <body>
+        
+    <header>
+        <DashboardHeader></DashboardHeader>
+    </header>
+    <p>Ваше имя {{ this.$store.getters.getUserName }}</p>
+    <p>Ваша Фамилия {{ this.$store.getters.getUserSurname }}</p>
+    <p>Ваша почта {{ this.$store.getters.getUserEmail }}</p>
+    <p>Ваш пароль {{ this.$store.getters.getUserPassword }}</p>
+</body>
 </template>
 
 <style scoped>
+body{
+    background-color: rgb(213, 244, 206);
+}
+
 p{
     text-align: center;
     color: brown;
---
+++ Law-simulator/src/components/subcomponents/DashboardHeader.vue
@@ -0,0 +1,55 @@
+<script>
+// export default{
+
+// }
+</script>
+
+<template>
+    <div class="header">
+        <div class="leftSide">
+            <img class="logo" src="@/components/icons/5269101_book_education_library_notebook_read_icon.svg" alt="Logo">
+            <p class="name">Правовой тренажёр</p>
+        </div>
+        <P> Пройти кейсы</P>
+        <P> Прогресс</P>
+        <div class="rightSide">
+            <img class="avatar" src="@/components/icons/9026108_user_icon.svg" alt="avatar">
+            <p>Личный кабинет</p>
+        </div>
+        
+    </div>
+</template>
+
+<style scoped>
+
+.header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #39a56d;
+}
+
+.logo{
+    height: 8vh;
+}
+
+.leftSide{
+    display: flex;
+    align-items: center;
+    margin-left: 10px;
+    
+}
+p{
+    font-size: 20px;
+    color: white;
+}
+.rightSide{
+    display: flex;  
+    align-items: center;
+    margin-right: 20px;
+}
+.avatar{
+    height: 3vw;
+    margin: 4px;
+}
+</style>
